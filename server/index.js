38a17d2fa9692f5f681631c54e2f4b7e6124304a
Ankit Mishra <ankitmishra545@gmail.com>
2024-08-05 22:22:36 +0530

    type changed to module

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const systemInfo = require("systeminformation");
-const path = require("path");
-
-const app = express();
+import express from "express";
+import cors from "cors";
+import systemInfo from "systeminformation";
+import path from "path";
 
 const __dirname = path.resolve();
+const app = express();
+
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
