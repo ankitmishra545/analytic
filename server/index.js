a49a15d0f01d659da2f6bdcbbae10d7aae94af84
Ankit Mishra <ankitmishra545@gmail.com>
2024-08-05 23:13:38 +0530

    new change

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ import path from "path";
 const __dirname = path.resolve();
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.use(cors());
